c1c3ad48ebc63aef2a1652f9c9f48952f05d07ac
Thiago Bignotto <bignotto@gmail.com>
2020-06-23 23:48:15 -0300

    create appointment route - video 2

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
-
+app.use(express.json());
+app.use(routes);
 app.get('/', (request, response) => {
     return response.json({ message: 'Big Rocks!' });
 });
