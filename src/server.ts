18217cd8f4823744c43f3b58415f75c0d0f3ff47
Thiago Bignotto <bignotto@gmail.com>
2020-06-23 22:43:04 -0300

    project configs

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-import express from "express";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Big Rocks!" });
+app.get('/', (request, response) => {
+    return response.json({ message: 'Big Rocks!' });
 });
 
 app.listen(3333, () => {
-  console.log("🚀 Lift off!!!!!!!!!!!!!!!!");
+    console.log('🚀 Lift off!!!!!!!!!!!!!!!!');
 });
