04cdfdabb4c04a2c905c95785affdb7099aa59c3
Thiago Bignotto <bignotto@gmail.com>
2020-08-30 02:30:48 -0300

    added handlebars as mail template provider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,6 +4,8 @@ import IMailProvider from './MailProvider/models/IMailProvider';
 
 import { container } from 'tsyringe';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
 container.registerSingleton<IStorageProvider>(
     'IStorageProvider',
@@ -13,4 +15,9 @@ container.registerSingleton<IStorageProvider>(
 container.registerInstance<IMailProvider>(
     'MailProvider',
     new EtherealMailProvider(),
+);
+
+container.registerSingleton<IMailTemplateProvider>(
+    'MailTemplateProvider',
+    HandlebarsMailTemplateProvider,
 );
