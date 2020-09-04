4c6cb6a2812c458e41c39fab824f22ff8478065e
Thiago Bignotto <bignotto@gmail.com>
2020-09-04 00:21:15 -0300

    wip: create list providers appointments service

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,50 @@
+import 'reflect-metadata';
+
+// import AuthenticateUserService from './AuthenticateUserService';
+import ListProviderAppointmentsService from '././ListProviderAppointmentsService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentRepository';
+
+import AppError from '@shared/errors/AppError';
+
+describe('ListProviderAppointments', () => {
+    let listProviderAppointmentsService: ListProviderAppointmentsService;
+    let fakeAppointmentRepository: FakeAppointmentsRepository;
+
+    beforeEach(() => {
+        fakeAppointmentRepository = new FakeAppointmentsRepository();
+        listProviderAppointmentsService = new ListProviderAppointmentsService(
+            fakeAppointmentRepository,
+        );
+    });
+
+    it('should list appointments on a given date', async () => {
+        const appointment1 = await fakeAppointmentRepository.create({
+            provider_id: 'provider 1',
+            user_id: 'user x',
+            date: new Date(2020, 9, 10, 9, 0, 0),
+        });
+        const appointment2 = await fakeAppointmentRepository.create({
+            provider_id: 'provider 1',
+            user_id: 'user x',
+            date: new Date(2020, 9, 10, 13, 0, 0),
+        });
+        const appointment3 = await fakeAppointmentRepository.create({
+            provider_id: 'provider 1',
+            user_id: 'user x',
+            date: new Date(2020, 9, 10, 17, 0, 0),
+        });
+
+        const appointments = await listProviderAppointmentsService.execute({
+            provider_id: 'provider 1',
+            year: 2020,
+            month: 10,
+            day: 10,
+        });
+
+        expect(appointments).toEqual([
+            appointment1,
+            appointment2,
+            appointment3,
+        ]);
+    });
+});
